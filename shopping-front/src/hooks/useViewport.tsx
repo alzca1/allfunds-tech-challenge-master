7f03a896c29f7460335fa1bb743e989cdcd3f420
Álvaro Zájara <alvaro.zajara@eviden.com>
2025-01-18 20:33:05 +0100

    added hook to control viewport size

---
+++ shopping-front/src/hooks/useViewport.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export const useViewPort = (): boolean => {
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window?.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return isMobile;
+};
