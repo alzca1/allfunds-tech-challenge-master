4cf3fd656ce01bb7e5cc5fd5c33543d5d9c56760
Álvaro Zájara <alvaro.zajara@eviden.com>
2025-01-18 08:16:38 +0100

    added ProductCard component initial structure and styling

---
+++ shopping-front/src/ProductCard/ProductCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { GroceryItem } from "../types/global.types";
+
+interface ProductCardProps {
+  itemDetails: GroceryItem;
+  handleAddItemToCart: (itemDetails: GroceryItem) => void;
+}
+
+export default function ProductCard({ itemDetails, handleAddItemToCart }: ProductCardProps) {
+  const { id, image_url, stock, productName, price, productDescription } = itemDetails;
+  return (
+    <div className="product-card">
+      <div className="img-container">
+        <img src={image_url} />
+      </div>
+      <div className="product-first-row">
+        <h6>{productName}</h6>
+        <span>{price}</span>
+      </div>
+      <div className="product-second-row">
+        <p>{productDescription}</p>
+      </div>
+      <div className="product-third-row">
+        <span>{stock} left</span>
+        <button onClick={() => handleAddItemToCart(itemDetails)}>+ add</button>
+      </div>
+    </div>
+  );
+}
