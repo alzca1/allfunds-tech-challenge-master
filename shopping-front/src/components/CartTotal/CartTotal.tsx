93e9b9f1707367066ddec729c0106f269cde0c91
Álvaro Zájara <alvaro.zajara@eviden.com>
2025-01-19 10:11:28 +0100

    made some modifications to CartTotal and Cart to adapt to multiple view layout

--- shopping-front/src/components/CartTotal/CartTotal.tsx
+++ shopping-front/src/components/CartTotal/CartTotal.tsx
@@ -5,21 +5,16 @@ interface CartTotalProps {
   cartItems: CartItem[];
 }
 
-const mockCartItems: CartItem[] = [
-  { price: 4.99, quantity: 2 },
-  { price: 7.59, quantity: 1 },
-  { price: 2.99, quantity: 4 },
-];
 export default function CartTotal({ cartItems }: CartTotalProps) {
-  //   const total: number = cartItems.reduce(
-  const total: number = mockCartItems.reduce(
+  const total: number = cartItems.reduce(
     (sum: number, item: CartItem) => sum + item?.price * item?.quantity,
     0
   );
 
   return (
     <div className="cart-total">
-      <h4>`Checkout ${total.toFixed(2)}`</h4>
+      <h4>Checkout</h4>
+      <h4>{`${total.toFixed(2)}`}€</h4>
     </div>
   );
 }
