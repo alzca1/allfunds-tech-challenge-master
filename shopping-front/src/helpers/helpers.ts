d88391688d923723787038cb82f97b54192565b4
Álvaro Zájara <alvaro.zajara@eviden.com>
2025-01-19 11:41:54 +0100

    enhanced truncateText function and added unit tests

--- shopping-front/src/helpers/helpers.ts
+++ shopping-front/src/helpers/helpers.ts
@@ -1,6 +1,8 @@
 export const truncateText = (text: string, numCharacters: number): string => {
+  if (!text || typeof numCharacters == "undefined") return text;
+
   if (text?.length <= numCharacters) {
     return text; // Devuelve el texto original si no supera la longitud máxima
   }
-  return text.slice(0, numCharacters) + "...";
+  return text?.slice(0, numCharacters) + "...";
 };
