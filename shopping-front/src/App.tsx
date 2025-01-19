c0c6db585ad7e77cea60a4db59e52e38860e78bb
Álvaro Zájara <alvaro.zajara@eviden.com>
2025-01-19 19:01:43 +0100

    restructured logic in App to handling items addition in the cart while updating item stock in the backend

--- shopping-front/src/App.tsx
+++ shopping-front/src/App.tsx
@@ -1,4 +1,4 @@
-import { Product, UpdateProductOperation } from "./types/global.types";
+import { CartItem, Product, UpdateProductOperation } from "./types/global.types";
 import ProductList from "./components/ProductList/ProductList";
 import { useEffect, useState } from "react";
 import { useViewPort } from "./hooks/useViewport";
@@ -9,13 +9,9 @@ function App() {
   const isMobile: boolean = useViewPort();
 
   const [isCheckoutVisible, setIsCheckoutVisible] = useState<boolean>(false);
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   const { getProducts, productDetails, updateProductStock } = useProducts();
-
-  const handleToggleCart = (): void => {
-    setIsCheckoutVisible((prevState) => !prevState);
-  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -25,6 +21,49 @@ function App() {
     fetchProducts();
   }, []);
 
+  const handleToggleCart = (): void => {
+    setIsCheckoutVisible((prevState) => !prevState);
+  };
+
+  const handleAddItemToCart = async (
+    itemDetails: Product,
+    operation: UpdateProductOperation,
+    amount: number
+  ): Promise<void> => {
+    const update = await updateProductStock(itemDetails?.id, operation, amount);
+    const hasCartItem = cartItems.some((cartItem: CartItem) => cartItem?.id === itemDetails?.id);
+    const cartItemsCopy: CartItem[] = structuredClone(cartItems);
+
+    if (update && !hasCartItem) {
+      const item: CartItem = {
+        id: itemDetails?.id,
+        image_url: itemDetails?.image_url,
+        productName: itemDetails?.productName,
+        quantity: 1,
+        price: itemDetails?.price,
+      };
+
+      cartItemsCopy.push(item);
+      setCartItems(cartItemsCopy);
+      return;
+    }
+
+    if (update && hasCartItem) {
+      const updatedCartItems: CartItem[] = cartItemsCopy?.map((cartItem: CartItem) => {
+        if (cartItem?.id === itemDetails?.id) {
+          return {
+            ...cartItem,
+            quantity: (cartItem.quantity += 1),
+          };
+        }
+        return cartItem;
+      });
+
+      setCartItems(updatedCartItems);
+      return;
+    }
+  };
+
   return (
     <div className="app">
       {isMobile ? (
@@ -32,7 +71,7 @@ function App() {
           <ProductList
             isLoading={productDetails?.isLoading}
             products={productDetails?.data}
-            handleAddItemToCart={updateProductStock}
+            handleAddItemToCart={handleAddItemToCart}
           />
           <div
             className={`${isCheckoutVisible ? "mobile-cart visible" : "mobile-cart not-visible"}`}
@@ -50,7 +89,7 @@ function App() {
           <ProductList
             products={productDetails?.data}
             isLoading={productDetails?.isLoading}
-            handleAddItemToCart={updateProductStock}
+            handleAddItemToCart={handleAddItemToCart}
           />
           <Cart cartItems={cartItems} />
         </div>
