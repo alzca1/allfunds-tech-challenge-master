fcdf14366171f244135aebc3a07ca87fca0f6624
Álvaro Zájara <alvaro.zajara@eviden.com>
2025-01-20 10:26:05 +0100

    removed unnecessary logic

--- shopping-front/src/App.tsx
+++ shopping-front/src/App.tsx
@@ -28,35 +28,6 @@ function App() {
   const handleUpdateCart = (cartItems: CartItem[]) => {
     setCartItems(cartItems);
   };
-  //   const update = await updateProductStock(id, UpdateProductOperation.SUBTRACT, 1);
-  //   const hasItemQuantityLeft = cartItems.some(
-  //     (cartItem: CartItem) => cartItem?.id == id && cartItem?.quantity - 1 > 0
-  //   );
-  //   const cartItemsCopy: CartItem[] = structuredClone(cartItems);
-
-  //   if (update && hasItemQuantityLeft) {
-  //     const updatedCartItems: CartItem[] = cartItemsCopy?.map((cartItem: CartItem) => {
-  //       if (cartItem?.id === id) {
-  //         const currentQuantity = cartItem?.quantity;
-  //         return {
-  //           ...cartItem,
-  //           quantity: currentQuantity - 1,
-  //         };
-  //       }
-  //       return cartItem;
-  //     });
-  //     setCartItems(updatedCartItems);
-  //     return;
-  //   }
-  //   if (update && !hasItemQuantityLeft) {
-  //     const updatedCartItems: CartItem[] = cartItemsCopy?.filter(
-  //       (cartItem: CartItem) => cartItem?.id !== id
-  //     );
-
-  //     setCartItems(updatedCartItems);
-  //     return;
-  //   }
-  // };
 
   return (
     <div className="app">
